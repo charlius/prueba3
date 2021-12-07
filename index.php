5b7a43ace4f055f2746626c65c4f0e47c5710a14
charlius <charlshsx@gmail.com>
2021-12-07 15:59:17 -0300

    index post

--- index.php
+++ index.php
@@ -181,6 +181,7 @@
        <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
 <!-- -->
  
+
 </div>
 <footer>
     <center><p>MuySocial | 2021.</p></center>
